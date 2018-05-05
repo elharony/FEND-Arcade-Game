3db63172617e576891fca35a0028216e07a030f0
elharony <p.yahyaelharony@gmail.com>
2018-05-05 18:21:11 +0200

    Move the 'Enemies' functionality

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function(x, y) {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = x;
     this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -12,9 +13,11 @@ var Enemy = function(x, y) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    if(this.x > 600) {
+        this.x = -100;
+    } else {
+        this.x += 100 * this.speed * dt;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -67,9 +70,9 @@ Player.prototype.handleInput = function(keyCode) {
 
 
 // Now instantiate your objects.
-var insect1 = new Enemy(0, 60);
-var insect2 = new Enemy(0, 145);
-var insect3 = new Enemy(0, 230);
+var insect1 = new Enemy(0, 60, 1);
+var insect2 = new Enemy(0, 145, 2);
+var insect3 = new Enemy(0, 230, 3);
 // Place all enemy objects in an array called allEnemies
 const allEnemies = [insect1, insect2, insect3];
 // Place the player object in a variable called player
